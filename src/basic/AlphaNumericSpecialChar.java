e1d8c8eeed6e4b220dd55c4f9d0ba1b63737051d
Pruthvy <Pruthvy@DESKTOP-I06O3L7>
2016-04-03 11:40:18 +0530

    Second Comit

--- src/basic/AlphaNumericSpecialChar.java
+++ src/basic/AlphaNumericSpecialChar.java
@@ -2,8 +2,9 @@ package basic;
 
 public class AlphaNumericSpecialChar {
 String s="nankAands12/";
+@SuppressWarnings("unused")
 public static void main(String[] args) {
 	int ch;
-	if(char ch=65<= && =>90)
+	//if(char ch=65<= && =>90)
 }
 }
